c11af3b73499e019fd7d2f4de680de820c5cac35
Gregory Dort <gregory.dort@gmail.com>
2025-09-17 08:59:20 -0400

    Started Header Logic

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
 const Navbar = () => {
+    // Function to handle authentication state added here later
 
+    // Function to sign out of account added here later
+    const handleSignOut = () => {
+        // sign out logic here
+    }
+
+    return (
+        <div className = "fixed top-0 left-0bg-transparent w-full flex shadow-md justify-between items-center p-6 z-50">
+            {/* Left Side of Navbar */}
+            <div className = "flex-grow flex justify-start items-center">
+                <h1 className = "text-xl text-gray-800">Welcome to PrepTime!</h1>
+            </div>
+
+            {/* Center of Navbar */}
+            <div className = "flex-none">
+                <img src="/logo.png" alt="PrepTime Logo" className = "h-10 w-auto"/>
+            </div>
+
+            {/* Right Side of Navbar */}
+            <div className = "flex-grow flex justify-end items-center space-x-4">
+                {/* Needs conditional rendering later on. Currently adding all buttons needed to the Navbar and will fix later*/}
+                <button
+                    onClick = {handleSignOut}
+                    className = "text-black rounded-md bg-white hover:bg-gradient-to-br from-yellow-100-to-orange-400 transition duration-300 px-4 py-2 font-medium"
+                >Sign Out</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,11 @@ import { useState } from 'react';
 
 const Home = () => {
     
+    return (
+        <div className = "bg-white min-h-screen flex flex-col">
+            
+        </div>
+    )
 }
 
 export default Home;
